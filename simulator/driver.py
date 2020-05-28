4193e8be3af311e5044eabdb13288a3e9e47aa65
Kirill Ershov <kershov@thetads.ru>
2020-05-28 17:50:09 +0300

    Change logic of movement - idle and repos are moving according to self.route - dict (key=sim seconds, value=hex) Add reposition and idle movement methods to collection - handling decisions of agent/model

--- simulator/driver.py
+++ simulator/driver.py
@@ -39,10 +39,29 @@ class DriversCollection(list):
     def get_reposition_drivers(self, n_drivers: int):
         idle_drivers = self.get_drivers(status='idle')
         for_reposition = [i for i in idle_drivers if i.idle_time >= self.env.VALID_REPOSITION_TIME]
-        return for_reposition[n_drivers]
+        return for_reposition[:n_drivers]
+
+    def reposition(self, agent_response: list):
+        for resp in agent_response:
+            driver = self.get_by_driver_id(resp['driver_id'])
+            driver.route = self.env.map.calculate_path(driver.driver_location, resp['destination'])
+            driver.status = 'reposition'
+            driver.idle_time = 0
+
+    def idle_movement(self, model_response: list):
+        for resp in model_response:
+            driver = self.get_by_driver_id(resp['driver_id'])
+            driver.route = self.env.map.calculate_path(driver.driver_location, resp['idle_hex'])
 
     def get_dispatching_drivers(self):
-        return self.get_drivers(status='idle')
+        return self.get_drivers(status='idle') + self.get_drivers(status='reposition')
+
+    def get_by_driver_id(self, driver_id):
+        drivers = {i.driver_id: i for i in self}
+        if driver_id not in drivers.keys():
+            raise DriversCollectionException(f'Driver_id={driver_id} does not exists')
+        else:
+            return drivers[driver_id]
 
 
 class Driver:
@@ -56,12 +75,8 @@ class Driver:
         self.driver_reward = 0
         self.status = 'idle'
         self.order = None
-        self.next_idle_location = [self.driver_location]
+        self.route = {self.env.current_seconds: self.driver_location}
         self.idle_time = abs(int(np.random.normal(300, 200)))
-
-    @property
-    def driver_grid(self):
-        return self.env.map.get_grid(self.driver_location)
 
     def take_order(self, order_object, reward_units: float, pick_up_eta: float,
                    order_finish_timestamp: int, order_driver_distance: float):
@@ -78,18 +93,26 @@ class Driver:
             self.idle_time = 0
 
     def move(self):
-        if self.status == 'idle':
-            self.driver_location = self.next_idle_location[0] #?
-            del self.next_idle_location[0]
-            self.idle_time += self.env.params.STEP_UNIT
-        elif self.status == 'assigned':
+        if self.status != 'assigned':
+            self._move()
+            if self.status == 'idle':
+                self.idle_time += self.env.params.STEP_UNIT
+            if self.status == 'reposition' and not self.route:
+                self.status = 'idle'
+        else:
             if self.env.timestamp == self.order.order_finish_timestamp:
                 self.driver_reward += self.order.reward
                 self.env.total_reward += self.order.reward
-                self.status = 'idle'
                 self.driver_location = self.order.finish_location
-        else:
+                self.env.orders_collection.cancel_orders([self.order])
+
+    def _move(self):
+        next_location = self.route.get(self.env.current_seconds)
+        if not next_location:
             pass
+        else:
+            self.driver_location = next_location
+            del self.route[self.env.current_seconds]
 
     def cancel_order(self):
         if self.status == 'assigned':
