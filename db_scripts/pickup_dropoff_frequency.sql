6775683f12a67870026842f23e79827ea5541c59
alexander.kleban <alexander.kleban@zyfra.com>
2020-05-28 18:08:40 +0300

    Change scripts

--- db_scripts/pickup_dropoff_frequency.sql
+++ db_scripts/pickup_dropoff_frequency.sql
@@ -1,7 +1,7 @@
 SELECT pickup_grid
      , dropoff_grid
-     , Case when pickup_weekday > 5 then 1 else 0 end as pickup_weekend
+     , pickup_weekday
      , pickup_hour
      , count(*)                                       as pickup_cnt
 FROM calc.ride_request_data_calc
-group by pickup_grid, dropoff_grid, Case when pickup_weekday > 5 then 1 else 0 end, pickup_hour
+group by pickup_grid, dropoff_grid, pickup_weekday, pickup_hour
--- db_scripts/pickup_lambdas.sql
+++ db_scripts/pickup_lambdas.sql
@@ -1,13 +1,10 @@
-with tab as (SELECT pickup_grid
-                  , EXTRACT(DAY FROM ride_start_time)              as pickup_day
-                  , Case when pickup_weekday > 5 then 1 else 0 end as pickup_weekend
+with tab as (SELECT pickup_weekday
                   , pickup_hour
                   , count(*)                                       as pickup_cnt
              FROM calc.ride_request_data_calc
-             group by pickup_grid, pickup_weekday, pickup_hour, EXTRACT(DAY FROM ride_start_time))
-SELECT pickup_grid
-     , pickup_weekend
+             group by pickup_weekday, pickup_hour, EXTRACT(DAY FROM ride_start_time))
+SELECT pickup_weekday
      , pickup_hour
      , avg(pickup_cnt) as pickup_cnt_avg
 FROM tab
-group by pickup_grid, pickup_weekend, pickup_hour
+group by pickup_weekday, pickup_hour
