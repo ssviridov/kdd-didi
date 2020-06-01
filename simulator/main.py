5cc413ca45aee86cffe01bf64820cfaf104f151f
Kirill Ershov <kershov@thetads.ru>
2020-06-01 18:31:10 +0300

    Add DataManager to simulator

--- simulator/main.py
+++ simulator/main.py
@@ -2,6 +2,7 @@ import json
 
 from .environment import Environment
 from .agent import Agent
+from .utils import DataManager
 
 import logging
 from logging import config
@@ -14,11 +15,16 @@ logger = logging.getLogger(__name__)
 class TaxiSimulator:
     day_seconds = 60 * 60 * 24
 
-    def __init__(self):
-        pass
+    def __init__(self, write_simulations_to_db=True):
+        if write_simulations_to_db:
+            self.db_client = DataManager()
+        else:
+            self.db_client = None
 
-    def simulate(self, day_of_week: int, agent: Agent):
-        env = Environment(day_of_week=day_of_week, agent=agent)
+    def simulate(self, day_of_week: int, agent: Agent, simulation_name=None):
+        if self.db_client:
+            self.db_client(simulation_name)
+        env = Environment(day_of_week=day_of_week, agent=agent, db_client=self.db_client)
         env.generate_orders()
         for sec in range(1, self.day_seconds + 1):
             env.update_current_time(current_seconds=sec)
@@ -28,5 +34,11 @@ class TaxiSimulator:
             env.balancing_drivers()
             if sec % 2 == 0:
                 env.dispatching_actions()
-                env.cancel_orders()
             env.move_drivers()
+            env.datacollector.write_simulation_step()
+        if not self.db_client:
+            return env.datacollector.data
+
+    def get_simulation(self, name: str):
+        return self.db_client.read_simulation(name)
+
