32326c7092382350e758daa3bb87a7c4b91161ec
alexander.kleban <alexander.kleban@zyfra.com>
2020-06-01 12:28:36 +0300

    Add logger for dispatching

--- simulator/utils.py
+++ simulator/utils.py
@@ -1,23 +1,34 @@
 from models import RewardModel
 from geopy.distance import geodesic
+import logging
+
+logger = logging.getLogger(__name__)
 
 RMODEL = RewardModel()
 
 
 def _create_order_driver_pair(env, o, d):
+    logger.info("Prepare pair for order and driver")
     pair = dict(order_id=o.order_id, driver_id=d.driver_id, order_start_location=o.order_start_location,
                 order_finish_location=o.order_finish_location, driver_location=d.driver_location,
                 timestamp=env.timestamp, day_of_week=env.day_of_week)
+    logger.info("Get distance")
     pair['order_driver_distance'] = get_distance(d.driver_location, o.order_start_location) * 1000
+    logger.info("Get pickup eta")
     pair['pick_up_eta'] = pair['order_driver_distance'] / env.PICKUP_SPEED_M_PER_S
+    logger.info("Get order duration")
     order_duration = env.map.calculate_distance(o.start_hex, o.finish_hex) * 1000 / env.PICKUP_SPEED_M_PER_S
     pair['order_finish_timestamp'] = env.timestamp + int(pair['pick_up_eta']) + int(order_duration)
+    logger.info("Get reward")
     pair['reward_units'] = RMODEL.predict(pair)
     return pair
 
 
 def _pairs_for_order(o, env, drivers):
+    # logger.info("Get pairs for order")
     close_hexes = [i for i in env.map.graph.neighbors(o.start_hex)] + [o.start_hex]
+    # _n_drivers = len([0 for d in drivers if d.driver_hex in close_hexes])
+    # logger.info(f"Iterate {_n_drivers} drivers in {len(close_hexes)} hexagons")
     return [_create_order_driver_pair(env, o, d) for d in drivers if d.driver_hex in close_hexes]
 
 
@@ -28,6 +39,7 @@ def get_distance(start, finish):
 
 
 def prepare_dispatching_request(env, drivers, orders):
+    logger.info("Prepare dispatching request")
     pairs = list()
     for order in orders:
         order_pairs = _pairs_for_order(order, env, drivers)
@@ -36,6 +48,7 @@ def prepare_dispatching_request(env, drivers, orders):
 
 
 def handle_dispatching_response(env, agent_request, agent_response):
+    logger.info("Handle dispatching response")
     for r in agent_response:
         order_info = [i for i in agent_request if i['driver_id'] == r['driver_id']
                       and i['order_id'] == r['order_id']][0]
