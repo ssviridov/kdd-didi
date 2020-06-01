703edce6c4463b6f6b14249be5feb5a288b0e618
Kirill Ershov <kershov@thetads.ru>
2020-06-01 18:29:16 +0300

    Show possibility of whether to run simulator with DB or not (in-memory)

--- simulator/test.py
+++ simulator/test.py
@@ -1,5 +1,17 @@
 from agent import Agent
 from simulator import TaxiSimulator
 
-taxi_sim = TaxiSimulator()
-taxi_sim.simulate(day_of_week=1, agent=Agent())
+import json
+
+I_INSTALLED_MONGO_CONTAINER = True
+
+if I_INSTALLED_MONGO_CONTAINER:
+    taxi_sim = TaxiSimulator(write_simulations_to_db=True)
+    taxi_sim.simulate(day_of_week=1, agent=Agent(), simulation_name='test_monday')
+    simulation = taxi_sim.get_simulation(name='test_monday')
+else:
+    taxi_sim = TaxiSimulator(write_simulations_to_db=False)
+    simulation = taxi_sim.simulate(day_of_week=1, agent=Agent())
+
+with open('test_simulation.json', 'w') as f:
+    json.dump(simulation, f)
