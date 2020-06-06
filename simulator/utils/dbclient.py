21a8171f11f7fbdddf1e80deae5cbae3f20a60c5
Kirill Ershov <kershov@thetads.ru>
2020-06-06 15:14:10 +0300

    Write trajectories to it's own collection

--- simulator/utils/dbclient.py
+++ simulator/utils/dbclient.py
@@ -22,7 +22,9 @@ class DataManager:
         try:
             self.client = MongoClient(**config)
             self.simulations_db = self.client.simulations
-            self.db_names = {"simulations": self.simulations_db}
+            self.trajectories_db = self.client.trajectories
+            self.trajectories_collection = self.trajectories_db['trajectories']
+            self.db_names = {"simulations": self.simulations_db, "trajectories": self.trajectories_db}
         except Exception as e:
             raise DataManagerException("Unsuccessful connection to MongoDB - {}. "
                                        "Check if docker is alive by command 'docker ps | grep didi_collector' or set"
@@ -40,7 +42,7 @@ class DataManager:
 
     def create_simulation(self, simulation_name):
         if self.simulation_exists(simulation_name):
-            simulation_name += f"_{datetime.now}"  # TODO: send sim name back to connector
+            simulation_name += f"_{datetime.now}"
             if self.simulation_exists(simulation_name):
                 raise DataManagerException("{} simulation already exists".format(simulation_name))
         else:
@@ -55,11 +57,18 @@ class DataManager:
             raise DataManagerException(f"{simulation_name} does not exist. Please, use create_simulation method first")
         if 'step' not in step_data.keys():
             raise DataManagerException("Step data should contain 'step' key")
-        document = self._prepare_document(step_data, read_mode=False)
+        trajectories, stats = self._prepare_for_writing(step_data)
+        #     self._prepare_document(step_data['trajectories'])
+        # document = self._prepare_document(step_data['total'], read_mode=False)
         try:
-            self.simulations_db[simulation_name].insert_one(document)
+            self.simulations_db[simulation_name].insert_one(stats)
         except DuplicateKeyError:
             raise DataManagerException(f"Step {step_data['step']} has been already written in {simulation_name}")
+        if len(trajectories) == 0:
+            pass
+        else:
+            self.trajectories_collection.insert_many(trajectories)
+
 
     def write_simulation(self, simulation_name, simulation_data: list):
         if not self.simulation_exists(simulation_name):
@@ -85,6 +94,26 @@ class DataManager:
         if not self.simulation_exists(simulation_name):
             raise DataManagerException("{} simulation does not exist".format(simulation_name))
         self.simulations_db[simulation_name].drop()
+
+    def _prepare_for_writing(self, step_data: dict):
+        stats = self._prepare_document(dict(step=step_data['step'], total=step_data['total']), read_mode=False)
+        if len(step_data['trajectories']) == 0:
+            return [], stats
+        else:
+            trajectories = [self._prepare_trajectory(i) for i in step_data['trajectories']]
+            return trajectories, stats
+
+    @staticmethod
+    def _prepare_trajectory(traj):
+        start, end = traj['status_start'].split('_')[0], traj['status_end'].split('_')[0]
+        if start == end:
+            action = start
+        else:
+            action = 'idle'
+        return dict(trajectory_id=traj['traj_id_start'], day_of_week=traj['day_of_week_start'], t_start=traj['t_start'],
+                    hex_start=traj['hex_start'], lonlat_start=traj['loc_start'], t_end=traj['t_end'],
+                    hex_end=traj['hex_end'], lonlat_end=traj['loc_end'], action=action, reward=traj['reward_end'],
+                    done=traj['done_end'])
 
     @staticmethod
     def _prepare_document(document, read_mode=True):
